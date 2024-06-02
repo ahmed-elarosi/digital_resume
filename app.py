cbcffdca2c6c4e1f1dfe45d15489323bfc0fc1d6
Ahmed-Elarosi <cabry95@protonmail.com>
2024-06-02 23:07:12 +0200

    change hero image

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from googletrans import Translator  # For translation
 current_dir =Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Ahmed_Elarosi.pdf"
-profile_pic = current_dir / "assets" / "ahmed.jpg"
+profile_pic = current_dir / "assets" / "ahmed_hamdi.png"
 translator = Translator()  # Create a translator instance
 
 
